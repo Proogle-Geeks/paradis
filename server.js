e136dfc24c941670b3b741fc5f24bd4b7d7ff567
Omar Hakem <youtarawneh997@gmail.com>
2021-03-05 15:48:58 +0200

    setting up the home form

--- server.js
+++ server.js
@@ -9,13 +9,26 @@ const override = require('method-override');
 
 // installing - configration
 const app = express();
-
+app.use(cors());
 require('dotenv').config();
 app.use(override('_method'));
 
 const client = new pg.Client(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT;
+
+// view-static
+app.set('view engine', 'ejs');
+app.use('/public', express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
+
+// handler funcitons
+const renderHome = (req, res) => {
+  res.render('index');
+};
+
+// paths-routs
+app.get('/', renderHome);
 
 app.listen(PORT, () => {
   console.log('The app is listening on port: ', PORT);
