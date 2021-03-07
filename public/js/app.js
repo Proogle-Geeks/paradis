623e4cc1eb083e00428352da44d16d6818377386
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-03-07 13:09:46 +0200

    done with home page 03

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,20 @@
 'use strict'
 
-$(".in").click(function () {
-  $(".burger").slideToggle();
-});
 
+
+
+
+$( document ).ready(function() {
+  $(".in").click(function () {
+    $(".burger").toggle(1800);
+  });
+  $(".in").click(function () {
+    $(".burger-outer").toggle(1400);
+  });
+  $(".in").click(function () {
+    $(".burger-outer-big").toggle(1000);
+  });
+});
 
 
 
