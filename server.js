634cc6378770ce8879e874c9f82760dc1da37992
Omar Hakem <youtarawneh997@gmail.com>
2021-03-07 13:33:54 +0200

    adding the top rated animes to the home page

--- server.js
+++ server.js
@@ -1,39 +1,154 @@
 'use strict';
 
-
+// importing
 const express = require('express');
 const cors = require('cors');
-const ejs = require('ejs');
+const superagent = require('superagent');
+const pg = require('pg');
+const override = require('method-override');
 
-
-
-
-require('dotenv').config();
+// installing - configration
 const app = express();
 app.use(cors());
-app.use('/public', express.static('./public'));
+require('dotenv').config();
+app.use(override('_method'));
 
+// const client = new pg.Client(process.env.DATABASE_URL);
 
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 const PORT = process.env.PORT;
 
-
-
+// view-static
 app.set('view engine', 'ejs');
-app.get('/',renderHome);
+app.use('/public', express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
 
+// handler funcitons
 
-function renderHome(req,res) {
-  res.render('index');
+// render index.ejs in the home page
+const renderHome = (req, res) => {
+  getTopAnimes().then((data) => {
+    getNewsData().then((animeNews) => {
+      console.log(data);
+      res.render('index', { anime: data, news: animeNews });
+    });
+  });
+};
+
+// show result of search in the search page
+const handleSearch = (req, res) => {
+  let anime = req.query.anime;
+  getAnimeData(anime).then((data) => {
+    res.render('searches/show', { anime: data });
+  });
+};
+
+// handle the details for anime
+
+const handleDetails = (req, res) => {
+  let anime = req.query.anime;
+  getAnimeTrailer(anime).then((data) => {
+    res.render('searches/detail', { anime: data });
+  });
+};
+
+// paths-routs
+app.get('/', renderHome);
+app.get('/search', handleSearch);
+app.get('/search/details', handleDetails);
+
+// functions
+// get anime data that the user search for
+const getAnimeData = (anime) => {
+  const query = { q: anime };
+  const url = 'https://api.jikan.moe/v3/search/anime';
+  return superagent
+    .get(url)
+    .query(query)
+    .then((data) => {
+      return data.body.results.map((element) => new Anime(element));
+    })
+    .catch((error) => {
+      console.log('Error in getting data from Jikan API: ', error);
+    });
+};
+// get top rated anime
+const getTopAnimes = () => {
+  const url = 'https://api.jikan.moe/v3/top/anime/1';
+  return superagent.get(url).then((data) => {
+    return data.body.top.map((element) => new Anime(element));
+  });
+};
+
+// get Anime trailer for youtube
+const getAnimeTrailer = (anime) => {
+  const url = 'https://youtube.googleapis.com/youtube/v3/search';
+  const query = {
+    part: 'snippet',
+    q: `${anime} trailer`,
+    key: process.env.YOUTUBE_KEY,
+  };
+  return superagent
+    .get(url)
+    .query(query)
+    .then((data) => {
+      return data.body.items[0].id.videoId;
+    })
+    .catch((error) => {
+      console.log('error from getting data from youtube API', error);
+    });
+};
+function getNewsData() {
+  // console.log(req);
+
+  let newsQuery = {
+    apiKey: process.env.apiKey,
+    q: 'anime',
+    qInTitle: 'anime',
+    language: 'en',
+    sortBy: 'popularity',
+    pageSize: 3,
+  };
+  let newsUrl = 'https://newsapi.org/v2/everything';
+  return superagent
+    .get(newsUrl)
+    .query(newsQuery)
+    .then((data) => {
+      let articles = data.body.articles;
+      return articles.map(
+        (article) =>
+          new News(
+            article.author,
+            article.title,
+            article.url,
+            article.urlToImage,
+            article.content
+          )
+      );
+    })
+    .catch((error) =>
+      console.log('error in getting news from News API: ', error)
+    );
 }
 
+// Constructors
 
+function Anime(anime) {
+  this.title = anime.title;
+  this.img_url = anime.image_url;
+  this.type = anime.type;
+  this.score = anime.score;
+  this.start_date = anime.start_date;
+  this.end_date = anime.end_date || 'Until Now';
+  this.rank = anime.rank;
+}
 
-
-
-
-
+function News(author, title, url, urlToImage, content) {
+  this.author = author;
+  this.title = title;
+  this.url = url;
+  this.urlToImage = urlToImage;
+  this.content = content;
+}
 app.listen(PORT, () => {
-    console.log('the app is listening to port ' + PORT);
+  console.log('The app is listening on port: ', PORT);
 });
-
